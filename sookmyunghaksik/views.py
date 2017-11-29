d46d7cb180f3ef1d4891ad7bab91d0d134b9a20c
seming <semin.j.0110@gmail.com>
2017-11-29 15:16:03 +0000

    /keyboard 빠진부분 추가

--- sookmyunghaksik/views.py
+++ sookmyunghaksik/views.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def keyboard(request):
+    keyboard = {
+        "type" : "buttons",
+        "buttons" : ["스노로즈 가기", "학식 확인", "버튼3"]
+    }
+
+    return JsonResponse(keyboard)
